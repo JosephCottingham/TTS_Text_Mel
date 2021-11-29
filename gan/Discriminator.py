8507bd6d778a99f5fcdb72528d43511233299e1e
Joseph Cottingham <joseph.cottingham@protonmail.com>
2021-11-29 12:02:41 -0500

    f

--- gan/Discriminator.py
+++ gan/Discriminator.py
@@ -3,11 +3,11 @@ from tensorflow.keras import layers
 
 def make_discriminator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Conv1D(64, (4, 4), strides=(2, 2), padding='same', input_shape=(32, 80, 1)))
+    model.add(layers.Conv1D(64, (4,), strides=(2, 2), padding='same', input_shape=(32, 80, 1)))
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.Conv1D(128, (4,), strides=(2, 2), padding='same'))
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
