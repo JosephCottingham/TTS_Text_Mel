b1672d9c2854bc41353e94895c08ccff417633b0
josephcottingham <joseph.h.cottingham@gmail.com>
2021-11-30 15:49:54 -0500

    f

--- gan/base_trainer.py
+++ gan/base_trainer.py
@@ -298,7 +298,8 @@ class GanBasedTrainer(BasedTrainer):
         self._check_train_finish()
 
     def _one_step_forward(self, batch):
-        per_replica_losses = self._strategy.run(self._one_step_forward_per_replica, args=(batch,))
+        self._strategy.run(self._one_step_forward_per_replica, args=(batch,))
+        per_replica_losses= .1
         return self._strategy.reduce(
             tf.distribute.ReduceOp.SUM, per_replica_losses, axis=None
         )
