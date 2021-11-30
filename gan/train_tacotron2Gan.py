903334d9093d53d08ef5fed6c7480ce98ba48ef4
josephcottingham <joseph.h.cottingham@gmail.com>
2021-11-29 21:15:27 -0500

    f

--- gan/train_tacotron2Gan.py
+++ gan/train_tacotron2Gan.py
@@ -175,7 +175,7 @@ class Tacotron2Trainer(GanBasedTrainer):
                 tf.ones_like(p), p, loss_fn=self.mse_loss
             )
             print('-', end=' ')
-            fake_loss += calculate_2d_loss(
+            fake_loss += calculate_3d_loss(
                 tf.zeros_like(p_hat[i]), p_hat[i], loss_fn=self.mse_loss
             )
             print('+', end=' ')
